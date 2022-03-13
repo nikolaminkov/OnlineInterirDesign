002c6549af749ac401278ef4565bffeddc696155
nikolaminkov <niki_nikmen@mail.bg>
2022-03-13 18:51:22 +0200

    Make the main page

---
+++ OneDrive/Desktop/MyProject/OnlineInterirDesign-main/OnlineInteriorDesign/OnlineInteriorDesign/wwwroot/css/linked.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OnlineInteriorDesign.wwwroot.css
+{
+    public class linkedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
